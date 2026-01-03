93479c35536e521b39ef293c7a8fccdda16784ba
Mahesh <maheshgite2811@gmail.com>
2026-01-03 20:09:57 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,25 +1,45 @@
 express=require("express");
-const {auth,userauth} = require("./middlewares/auth");
+
+const User=require("./Models/User"); 
+
+const {connectDB}=require("./config/database");
+// const {auth,userauth} = require("./middlewares/auth");
 
 app=express();
 
-app.get("/admin/getUserData",(req,res)=>{
-    try{
-        throw new Error("error occured");
-    }catch(err)
-    {
-        res.status(500).send("something wrong");
+app.post("/signup",async (req,res)=>{
+
+    //creating the new instance of User model 
+    const user=new User(
+        {
+        firstName:"avi",
+        lastName:"Gamer",
+        emailID:"gamer@gmail.com",
+        password:"game@123"
+       
     }
+    );
+
+  try {
+    await user.save();//save in the database
+    res.status(201).send("data saved successfully");
+} catch (err) {
+    res.status(500).send(err.message);
+}
+
 })
 
-
-app.use((err,req,res,next)=>{
-    if(err){
-        res.status(500).send("something went wrong");
-    }
-})
-
-app.listen(7777,()=>{
+connectDB().then(()=>{
+    console.log("Database connection estabilished....");
+    app.listen(7777,()=>{
     console.log("server is listening on port 7777.......");
 })
+}).catch((err)=>{
+    console.error("Database cannot be connected");
+})
+
+
+
+
+
 
---
+++ src/config/database.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose");
+
+
+const connectDB = async ()=>{
+    await mongoose.connect("mongodb+srv://Mahesh_Brand:VmB0o1lmrj6ROOHy@devtinderdbappserver.ymgcub3.mongodb.net/devtinder");
+}
+
+module.exports={connectDB};
